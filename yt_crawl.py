fafbf185ed8e681c266819f7a5ba706d71914250
Thomas Hobson <iamthomasnz@gmail.com>
2019-10-01 23:44:49 +1300

    Fixed a bug with regex breaking times

--- yt_crawl.py
+++ yt_crawl.py
@@ -1,10 +1,11 @@
 import requests
 import re
 import json
-#import pdb, jsontree # When Debugging
+# import pdb, jsontree # When Debugging
+
 
 class SearchEntry:
-    #JSON serializey stuff
+    # JSON serializey stuff
     class Timestamp:
         minutes = 0
         seconds = 1
@@ -28,32 +29,35 @@ class SearchEntry:
         }
 
 
-api_key = ''  # ADD YOUR YOUTUBE API KEY HERE !!
-                                                     # Dont commit to git :>
+# ADD YOUR YOUTUBE API KEY HERE !!
+api_key = ''    # Dont commit to git :>
 api_url = 'https://www.googleapis.com/youtube/v3/'
 channel_id = 'UCa6eh7gCkpPo5XXUDfygQQA'
+
 
 def GetUploadPlaylist():
     # YouTube API will only return a list of videos in a playlist, not channel.
     # This will get the playlist that contains all videos.
-    data = { 'id':channel_id,
-        'key':api_key,
-        'part':'contentDetails'}
+    data = {'id': channel_id,
+            'key': api_key,
+            'part': 'contentDetails'}
     r = requests.get(f'{api_url}channels', params=data)
     response = json.loads(r.text)
     # ToDo: There's gotta be a better way to do this...
-    upload_id = response.get('items')[0].get('contentDetails').get('relatedPlaylists').get('uploads')
+    upload_id = response.get('items')[0].get(
+        'contentDetails').get('relatedPlaylists').get('uploads')
     return upload_id
+
 
 def GetTotalVideosInPlaylist():
     # Get the total number of videos, so our playlist crawler knows how many videos to grab.
     # Probably is not needed, had created this before investigating how YouTube returns pages
     # in a query.
-    data = { 
-        'key':api_key,
-        'playlistId':GetUploadPlaylist(),
-        'part':'snippet',
-        'maxResults':'2'}
+    data = {
+        'key': api_key,
+        'playlistId': GetUploadPlaylist(),
+        'part': 'snippet',
+        'maxResults': '2'}
     r = requests.get(f'{api_url}playlistItems', params=data)
     response = json.loads(r.text)
     total_videos = response.get('pageInfo').get('totalResults')
@@ -62,22 +66,22 @@ def GetTotalVideosInPlaylist():
 
 def GetVideosInPlaylist():
     # Gets all the videos in a playlist, hardcoded to the Uploaded Playlist
-    #https://www.googleapis.com/youtube/v3/playlistItems?playlistId={"uploads" Id}&key={API key}&part=snippet&maxResults=50
+    # https://www.googleapis.com/youtube/v3/playlistItems?playlistId={"uploads" Id}&key={API key}&part=snippet&maxResults=50
     output = []
     next_page_token = ''
     page = 1
     total_videos = GetTotalVideosInPlaylist()
     # This logic probably can be replaced by doing checks against the nextPageToken.
     while total_videos > 0:
-        page +=1
+        page += 1
         total_videos = total_videos - 50
-        data = { 
-            'key':api_key,
-            'playlistId':GetUploadPlaylist(),
-            'part':'snippet',
-            'maxResults':'50'}
+        data = {
+            'key': api_key,
+            'playlistId': GetUploadPlaylist(),
+            'part': 'snippet',
+            'maxResults': '50'}
         if next_page_token:
-            data.update( {'pageToken':next_page_token} )
+            data.update({'pageToken': next_page_token})
         r = requests.get(f'{api_url}playlistItems', params=data)
         videos = json.loads(r.text)
         next_page_token = videos.get('nextPageToken')
@@ -86,8 +90,9 @@ def GetVideosInPlaylist():
             date = video.get('snippet').get('publishedAt')
             title = video.get('snippet').get('title')
             description = video.get('snippet').get('description')
-            output.append([date,vId,title,description])
+            output.append([date, vId, title, description])
     return output
+
 
 videos = []
 print("Grabbing video list")
@@ -99,23 +104,32 @@ for video in output:
         if "HackTheBox" in video[2] or "VulnHub" in video[2]:
             title = video[2].split()[2]
         if line != "":
-            if not re.search('$[\d]*:[\d]', line):
+            if not re.search('^\w[\d]*:[\d]', line):
                 line = '00:01 - ' + line
 
-            temp = line.split(" - ")
-            timestamp = temp[0].split(":")
-            seconds = timestamp[1]
-            minutes = timestamp[0]
-            line = " - ".join(temp[1::])
+            temp = line.split("-")
 
-            entry = SearchEntry(title, video[1], minutes, seconds, line).AsJsonSerializable()
+            timestamp = temp[0].strip().split(":")
+
+            seconds = timestamp[-1]
+            hours = 0
+            try:
+                hours = int(timestamp[-3])
+            except:
+                pass
+            minutes = int(timestamp[-2]) + int(hours * 60)
+
+            newline = "-".join(temp[1::])
+
+            entry = SearchEntry(
+                title, video[1], minutes, seconds, newline).AsJsonSerializable()
+
             videos.append(entry)
             #print(f'{title} | {video[1]} ^ {line}')
 
-print("Serializing dataset")            
+print("Serializing dataset")
 dataset = json.dumps(videos)
 print("Writing Dataset dataset...")
-with open("dataset.json","w") as ds:
+with open("dataset.json", "w") as ds:
     ds.write(dataset)
-print("Done! Now commit to git")            
-
+print("Done! Now commit to git")

30cadf390ef6708aa44473e7cfc808c7e873fef0
Thomas Hobson <iamthomasnz@gmail.com>
2019-10-02 14:05:19 +1300

    Cleaning code

--- logic.js
+++ logic.js
@@ -1,73 +1,99 @@
-var searchResultFormat = '<tr><td>$machine</td><td>$line</td><td><a href="$link">Open Link</a></td></tr>';
+var searchResultFormat = '<tr><td>$machine</td><td>$line</td><td><a href="$link">YouTube</a></td></tr>';
 var linkTemplate = 'https://youtube.com/watch?v=$video&start=$time';
+
+var controls = {
+	displayResults: function(){
+		$results.show();
+	},
+	hideResults: function(){
+		$results.hide();
+	},
+	doSearch: function(match, dataset){
+		results = [];
+
+		regex = match.toLowerCase();
+		dataset.forEach((e) => {
+			if (e.line.toLowerCase().match(regex) || e.machine.toLowerCase().match(regex)) results.push(e);
+		});
+		return results;
+	},
+	updateResults: function($loc, results){
+		$loc.empty();
+		$noResults.hide();
+		$resultsTableHideable.show();
+		if (results.length == 0) {
+			$noResults.show();
+			$resultsTableHideable.hide();
+		}
+
+		results.forEach((r) => {
+			//Not the fastest but it makes for easier to read code :>
+
+			timeInSeconds = r.timestamp.minutes * 60 + r.timestamp.seconds;
+			el = searchResultFormat
+				.replace('$machine', r.machine)
+				.replace('$line', r.line)
+				.replace('$link', linkTemplate.replace('$video', r.videoId).replace('$time', timeInSeconds));
+
+			$loc.append(el);
+		});
+	},
+	setColor: function($loc, indicator){
+		var colorTestRegex = /^color-/i;
+		$loc[0].classList.forEach((cls) => {
+			//we cant use class so we use cls instead :>
+			if (cls.match(colorTestRegex)) $loc.removeClass(cls);
+		});
+		$loc.addClass('color-' + indicator);
+	}
+};
+window.controls = controls;
 
 $(document).ready(() => {
 	$results = $('div#results');
 	$query = $('#query');
 	$searchValue = $('input#search').first();
 	$form = $('form#searchForm');
+	$resultsTableHideable = $('table#results');
 	$resultsTable = $('tbody#results').first();
 	$noResults = $('div.noResults');
+	$colorUpdate = $('body');
 
-	controls = {
-		displayResults: function(){
-			$query.hide();
-			$results.show();
-		},
-		hideResults: function(){
-			$results.hide();
-			$query.show();
-		},
-		doSearch: function(match){
-			results = [];
-			regex = match.toLowerCase();
-			window.dataset.forEach((e) => {
-				if (e.line.toLowerCase().match(regex) || e.machine.toLowerCase().match(regex)) results.push(e);
-			});
-			return results;
-		},
-		updateResults: function($loc, results){
-			$loc.empty();
-			$noResults.hide();
-			if (results.length == 0) {
-				$noResults.show();
-			}
+	controls.setColor($colorUpdate, 'no-search');
+	controls.hideResults();
+	var currentSet = [];
+	var oldSearchValue = '';
 
-			results.forEach((r) => {
-				//Not the fastest but it makes for easier to read code :>
+	function doSearch(event){
+		var val = $searchValue.val();
 
-				timeInSeconds = r.timestamp.minutes * 60 + r.timestamp.seconds;
-				el = searchResultFormat
-					.replace('$machine', r.machine)
-					.replace('$line', r.line)
-					.replace('$link', linkTemplate.replace('$video', r.videoId).replace('$time', timeInSeconds));
+		if (val != '') {
+			controls.displayResults();
+			if (val.length < oldSearchValue.length) currentSet = window.dataset;
+			oldSearchValue = val;
 
-				$loc.append(el);
-			});
+			currentSet = window.controls.doSearch(val, currentSet);
+
+			window.controls.setColor($colorUpdate, currentSet.length == 0 ? 'no-results' : 'results-found');
+
+			window.controls.updateResults($resultsTable, currentSet);
 		}
-	};
-	window.controls = controls;
+		else {
+			controls.hideResults();
+			window.controls.setColor($colorUpdate, 'no-search');
+		}
+
+		if (event.type == 'submit') event.preventDefault();
+	}
 
 	$.getJSON('./dataset.json', (data) => {
 		window.dataset = data;
-		controls.updateResults($resultsTable, window.dataset);
+		currentSet = window.dataset;
+		window.controls.updateResults($resultsTable, window.dataset);
+		doSearch({ type: 'none' });
 	});
 
-	$form.submit((event) => {
-		controls.displayResults();
-		//Dont send the form
-		if ($resultsTable.children().length == 0) {
-			val = $searchValue.val();
-			found = controls.doSearch(val);
-			controls.updateResults($resultsTable, found);
-		}
+	$form.submit(doSearch);
 
-		event.preventDefault();
-	});
-
-	$searchValue.on('input', function(){
-		val = $searchValue.val();
-		found = controls.doSearch(val);
-		controls.updateResults($resultsTable, found);
-	});
+	$searchValue.on('input', doSearch);
 });
